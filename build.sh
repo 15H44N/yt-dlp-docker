29c9490e91645a32f95db137a3f3b198dfd5f242
Ishaan Aditya <14124624+15H44N@users.noreply.github.com>
2024-08-22 21:34:50 +0530

    upgrade versions

--- build.sh
+++ build.sh
@@ -1,37 +1,32 @@
 #!/bin/bash
 
 # Repository Login
-echo -e "### login to docker.io ###"
-podman login docker.io
-
-echo -e "\n### login to quay.io ###"
-podman login quay.io
+# echo -e "### login to docker.io ###"
+# podman login docker.io
 
 # Building Image
 CURDIR=$PWD
+
 cd Containerfile.d
 
-for f in Containerfile*
-do
+for f in Containerfile*; do
     echo -e "\n### Build ${f/Containerfile./} ###"
-    buildah build --layers=true -t yt-dlp:"${f/Containerfile./}" -f $f
+    podman build --layers=true -t yt-dlp:"${f/Containerfile./}" -f $f
 done
 
 # Test
-for f in Containerfile*
-do
+for f in Containerfile*; do
     echo -e "\n### Run ${f/Containerfile./} ###"
-    podman run --rm yt-dlp:${f/Containerfile./}  --version
+    podman run --rm yt-dlp:${f/Containerfile./} --version
 done
 
 # Push
-for f in Containerfile*
-do
+for f in Containerfile*; do
     echo -e "\n### Push yt-dlp:${f/Containerfile./} ###"
-    podman push yt-dlp:${f/Containerfile./} docker.io/tnk4on/yt-dlp:${f/Containerfile./} --format v2s2
+    podman push yt-dlp:${f/Containerfile./} docker.io/ishn/yt-dlp:${f/Containerfile./} --format v2s2
     if [ "${f/Containerfile./}" = "alpine-static" ]; then
         echo -e "\n### Push yt-dlp:latest"
-        podman push yt-dlp:${f/Containerfile./} docker.io/tnk4on/yt-dlp --format v2s2
+        podman push yt-dlp:${f/Containerfile./} docker.io/ishn/yt-dlp --format v2s2
     fi
 done
 
